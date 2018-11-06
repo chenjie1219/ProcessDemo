c72b7badff672663a95b51559171890b78400818
chenjie <4234115@qq.com>
2018-11-06 17:36:16 +0800

    add Edit

--- ProcessDemo/ViewController.swift
+++ ProcessDemo/ViewController.swift
@@ -45,25 +45,27 @@ extension ViewController{
     //选择保存路径
     @IBAction func selectSavePath(_ sender: NSButton) {
         
-        //1.创建打开文档面板对象
+        //创建打开文档面板对象
         let openPanel = NSOpenPanel()
-        //2.设置确认按钮文字
+        //设置确认按钮文字
         openPanel.prompt = "Select"
-        //3.设置禁止选择文件
+        //设置禁止选择文件
         openPanel.canChooseFiles = false
-        //4.设置可以选择目录
+        //设置可以选择目录
         openPanel.canChooseDirectories = true
-        //5.弹出面板框
+        //设置可以创建文件夹
+        openPanel.canCreateDirectories = true
+        //弹出面板框
         openPanel.beginSheetModal(for: view.window!) { (result) in
-            //6.选择确认按钮
+            //选择确认按钮
             if result == .OK {
-                //7.获取选择的路径
+                //获取选择的路径
                 self.savePathLbl.stringValue = openPanel.directoryURL?.path ?? ""
-                //8.保存用户选择路径(为了可以在其他地方有权限访问这个路径，需要对用户选择的路径进行保存)
+                //保存用户选择路径(为了可以在其他地方有权限访问这个路径，需要对用户选择的路径进行保存)
                 UserDefaults.standard.setValue(openPanel.url?.path, forKey: kSelectedFilePath)
                 UserDefaults.standard.synchronize()
             }
-            //9.恢复按钮状态
+            //恢复按钮状态
             sender.state = .off
             
         }
